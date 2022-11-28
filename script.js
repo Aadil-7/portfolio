ad480f1a71e7bbe1a19e6e1b44da90727cb33dde
Aadil-7 <aadilbhusnur007@gmail.com>
2022-11-28 21:41:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let targets = document.querySelectorAll(`.imgholder`)
+let info = document.querySelectorAll(`.proInfo`)
+
+let observer = new IntersectionObserver(entries=>{
+     entries.forEach(entry=>{
+        entry.target.classList.toggle('show', entry.isIntersecting)
+        if(entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entry)
+     })
+})
+targets.forEach(target=>{
+    observer.observe(target)
+})
+info.forEach(target=>{
+    observer.observe(target)
+})
