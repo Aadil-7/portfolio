97d5e93427b1a941e26cf31ff1756016770b96c4
Aadil-7 <aadilbhusnur007@gmail.com>
2022-12-06 14:58:54 +0530

    form testing

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let targets = document.querySelectorAll(`.imgholder`)
 let info = document.querySelectorAll(`.proInfo`)
 
-let observer = new IntersectionObserver(entries=>{
-     entries.forEach(entry=>{
+let observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
         entry.target.classList.toggle('show', entry.isIntersecting)
-        if(entry.isIntersecting) observer.unobserve(entry.target)
-     })
+        if (entry.isIntersecting) observer.unobserve(entry.target)
+    })
 })
-targets.forEach(target=>{
+targets.forEach(target => {
     observer.observe(target)
 })
-info.forEach(target=>{
+info.forEach(target => {
     observer.observe(target)
 })
